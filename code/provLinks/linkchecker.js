26a225d9c85d1654436e519965a57b350754580f
canadajones68 <48071533+canadajones@users.noreply.github.com>
2019-10-11 22:24:11 +0200

    Finished up the link checker
    - Added support to the link checker for checking sea provinces
    - Deleted struct.json
    - Fixed provLinks.json so all the links are bidirectional
    - Moved all province link related files to their own folder

--- code/provLinks/linkchecker.js
+++ code/provLinks/linkchecker.js
@@ -3,9 +3,13 @@
 // Get 'fs' library
 const fs = require('fs');
 
+// Get 'os' library
+const os = require('os');
+
 // Get province object file
 const obj = require('./provLinks.json');
 
+// Define province list arrays
 const landProvs = Object.keys(obj.landProv);
 const seaProvs = Object.keys(obj.seaProv);
 
@@ -66,7 +70,7 @@ for (let i = 0; i < objLandLgt; i++) {
 		}
 		// Check if we're cheking a link towards a sea province
 		else if (obj.seaProv.hasOwnProperty(chkLink)) {
-			// Itearate through links of the 'chkLink' province
+			// Iterate through links of the 'chkLink' province
 			for (let iii = 0; iii< obj.seaProv[chkLink].length; iii++ ) {
 				// Check if link exists, if so log a 'c' and break
 				if (obj.seaProv[chkLink][iii] == curLink) {
@@ -88,8 +92,81 @@ for (let i = 0; i < objLandLgt; i++) {
 	}
 }
 
+// Reset persistent counter
 co = 0; 
 
+// Iterate through all the provinces in 'obj.seaProv'
+for (let i = 0; i < objSeaLgt; i++) {
+	
+	// Define 'curLink' to be the province we're checking the links of
+	let curLink = seaProvs[i];
+	
+	// Iterate through all the province links in the current province
+	for (let ii=0; ii < obj.seaProv[curLink].length; ii++) {
+		
 
-console.log(landProvChecks);
-console.log(seaProvChecks);
+		// Define chkLink to be the current link 
+		let chkLink = "";
+		chkLink = obj.seaProv[curLink][ii];
+		
+		// Add 'curLink' to the current string
+		seaProvChecks[co] = curLink;
+		
+		// Add '-' ot the current string
+		seaProvChecks[co] += '-';
+		
+		// Add 'chkLink' to the current string
+		seaProvChecks[co] += chkLink;
+		
+		// Add ': ' to the current string
+		seaProvChecks[co] += ': ';
+		
+		// Check if we're checking a link towards a land province
+		if (obj.landProv.hasOwnProperty(chkLink)) {
+			// Iterate through links of the 'chkLink' province
+			for (let iii = 0; iii< obj.landProv[chkLink].length; iii++ ) {
+				// Check if link exists, if so log a 'c' and break
+				if (obj.landProv[chkLink][iii] == curLink) {
+					seaProvChecks[co] += 'c';
+					break;
+				}
+				// If no province link has been found, log a 'v' and break
+				else if (iii == obj.landProv[chkLink].length - 1) {
+					seaProvChecks[co] += 'v';
+					break;
+				}
+			}
+		}
+		// Check if we're cheking a link towards a sea province
+		else if (obj.seaProv.hasOwnProperty(chkLink)) {
+			// Iterate through links of the 'chkLink' province
+			for (let iii = 0; iii< obj.seaProv[chkLink].length; iii++ ) {
+				// Check if link exists, if so log a 'c' and break
+				if (obj.seaProv[chkLink][iii] == curLink) {
+					seaProvChecks[co] += 'c';
+					break;
+				}
+				// If no province link has been found, log a 'v' and break
+				else if (iii == obj.seaProv[chkLink].length - 1) {
+					seaProvChecks[co] += 'v';
+					break;
+				}
+			}	
+		}
+		// If the 'chkLink' province doesn't exist, log the fault and continue the loop
+		else if (!(obj.landProv.hasOwnProperty(chkLink)) && !(obj.seaProv.hasOwnProperty(chkLink))) {
+			seaProvChecks[co] += 'Invalid province name';
+		}
+		co++;
+	}
+}
+let finalResult = 'Land province links: '+ os.EOL + JSON.stringify(landProvChecks, null, '\t') + os.EOL + os.EOL +  'Sea province links: ' + os.EOL + JSON.stringify(seaProvChecks, null, '\t');
+
+try {
+	fs.writeFileSync('linkTestResults.txt', finalResult);
+}
+catch(err) {
+	console.error(err);
+}
+
+console.log('Finished! See "linkTestResults.txt" for results!');
