cde86c9301f6695cab244e719f4cad05373638b8
canadajones68 <48071533+canadajones@users.noreply.github.com>
2019-10-11 19:27:27 +0200

    Added comments
    Commented what the code does

--- code/linkchecker.js
+++ code/linkchecker.js
@@ -48,7 +48,7 @@ for (let i = 0; i < objLandLgt; i++) {
 		// Add ': ' to the current string
 		landProvChecks[co] += ': ';
 		
-		// Check if we're checking a link towards a sea province
+		// Check if we're checking a link towards a land province
 		if (obj.landProv.hasOwnProperty(chkLink)) {
 			// Iterate through links of the 'chkLink' province
 			for (let iii = 0; iii< obj.landProv[chkLink].length; iii++ ) {
@@ -64,20 +64,25 @@ for (let i = 0; i < objLandLgt; i++) {
 				}
 			}
 		}
+		// Check if we're cheking a link towards a sea province
 		else if (obj.seaProv.hasOwnProperty(chkLink)) {
+			// Itearate through links of the 'chkLink' province
 			for (let iii = 0; iii< obj.seaProv[chkLink].length; iii++ ) {
+				// Check if link exists, if so log a 'c' and break
 				if (obj.seaProv[chkLink][iii] == curLink) {
 					landProvChecks[co] += 'c';
 					break;
 				}
+				// If no province link has been found, log a 'v' and break
 				else if (iii == obj.seaProv[chkLink].length - 1) {
 					landProvChecks[co] += 'v';
 					break;
 				}
 			}	
 		}
-		else {
-			landProvChecks[co] += 'Invalid province link!'
+		// If the 'chkLink' province doesn't exist, log the fault and continue the loop
+		else if (!(obj.landProv.hasOwnProperty(chkLink)) && !(obj.seaProv.hasOwnProperty(chkLink))) {
+			landProvChecks[co] += 'Invalid province name';
 		}
 		co++;
 	}
@@ -85,56 +90,6 @@ for (let i = 0; i < objLandLgt; i++) {
 
 co = 0; 
 
-for (let i = 0; i < objLandLgt; i++) {
-	
-	let curLink = landProvs[i];
-	for (let ii=0; ii < obj.landProv[curLink].length; ii++) {
-		
-		let chkLink = "";
-		chkLink = obj.landProv[curLink][ii];
-		
-		landProvChecks[co] = curLink;
-		
-		landProvChecks[co] += '-';
-		
-		landProvChecks[co] += chkLink;
-		
-		landProvChecks[co] += ': ';
-		console.log(chkLink);
-		if (obj.landProv.hasOwnProperty(chkLink)) {
-			for (let iii = 0; iii< obj.landProv[chkLink].length; iii++ ) {
-			
-				if (obj.landProv[chkLink][iii] == curLink) {
-					console.log('h' + co);
-					landProvChecks[co] += 'c';
-					break;
-				}
-				else if (iii == obj.landProv[chkLink].length - 1) {
-					console.log('i');
-					landProvChecks[co] += 'v';
-					break;
-				}
-			}
-		}
-		else if (obj.seaProv.hasOwnProperty(chkLink)) {
-			for (let iii = 0; iii< obj.seaProv[chkLink].length; iii++ ) {
-				if (obj.seaProv[chkLink][iii] == curLink) {
-					console.log('h' + co);
-					landProvChecks[co] += 'c';
-					break;
-				}
-				else if (iii == obj.seaProv[chkLink].length - 1) {
-					console.log('i');
-					landProvChecks[co] += 'v';
-					break;
-				}
-			}	
-		}
-	
-		console.log(co);
-		co++;
-	}
-console.log('i: ' + co);
-}
+
 console.log(landProvChecks);
 console.log(seaProvChecks);
